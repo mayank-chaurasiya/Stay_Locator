6e09236ddc26022d8f7c393460874fe37d1489b9
Mayank <mayankchaurasiya66@gmail.com>
2025-09-08 21:49:52 +0530

    updated things

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
+const Listing = require("./models/listing.js");
 
 const app = express();
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.set(express.static(path.join(__dirname, "/public")));
+app.use(express.urlencoded({ extended: true }));
 
 let PORT = 8000;
 
@@ -23,6 +29,33 @@ async function main() {
 app.get("/", (req, res) => {
   res.send("hii I am working!!");
 });
+
+//------------ GET ALL LISTINGS ---------------------------------
+app.get("/listings", async (req, res) => {
+  const allListings = await Listing.find({});
+  res.render("listings/all-listings.ejs", { allListings });
+});
+
+//----------- SHOW ROUTE ---------------------------------------
+app.get("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/show-listing.ejs", { listing });
+});
+
+// app.get("/testListing", async (req, res) => {
+//   let sampleListing = new Listing({
+//     title: "My new villa",
+//     description: "By the beach",
+//     price: 1200,
+//     location: "Calangute, Goa",
+//     country: "India",
+//   });
+
+//   await sampleListing.save();
+//   console.log("sample was saved");
+//   res.send("successfule testing");
+// });
 
 app.listen(PORT, () => {
   console.log(`app is listenning to ${PORT}`);
---
+++ models/listing.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema; // a variable name to store mongoose.schema
+
+const listingSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  image: {
+    url: String,
+    filename: String,
+
+    // set: (value) =>
+    //   value === ""
+    //     ? "https://images.unsplash.com/photo-1593915451045-064bf8007a0f?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    //     : value,
+  },
+  price: {
+    type: Number,
+  },
+  location: {
+    type: String,
+  },
+  country: {
+    type: String,
+  },
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
