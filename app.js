1d84c4db32eb29e3ec44bfaafb20807deed12d3a
Mayank <mayankchaurasiya66@gmail.com>
2025-09-16 13:56:22 +0530

    phase 1 part c

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const path = require("path");
 const Listing = require("./models/listing.js");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
+const { listingSchema } = require("./schema.js");
 
 const app = express();
 app.set("view engine", "ejs");
@@ -34,56 +37,88 @@ app.get("/", (req, res) => {
   res.send("hii I am working!!");
 });
 
+//----------- VALIDATION AS MIDDLEWARE --------------------------
+const validateListing = (req, res, next) => {
+  let { error } = listingSchema.validate(req.body);
+  if (error) {
+    let errMsg = error.details.map((element) => element.message).join(",");
+    throw new ExpressError(400, errMsg);
+  }
+};
+
 //------------ GET ALL LISTINGS ---------------------------------
-app.get("/listings", async (req, res) => {
-  const allListings = await Listing.find({});
-  res.render("listings/all-listings.ejs", { allListings });
-});
+app.get(
+  "/listings",
+  wrapAsync(async (req, res) => {
+    const allListings = await Listing.find({});
+    res.render("listings/all-listings.ejs", { allListings });
+  })
+);
 
 //---------- NEW ROUTE -----------------------------------------
-app.get("/listings/new", async (req, res) => {
-  res.render("listings/new-listing.ejs");
-});
+app.get(
+  "/listings/new",
+  wrapAsync(async (req, res) => {
+    res.render("listings/new-listing.ejs");
+  })
+);
 
 //---------- CREATE ROUTE --------------------------------------
-app.post("/listings", async (req, res) => {
-  let listingData = req.body.listing;
-  listingData.image = { url: listingData.image };
-  let newListing = new Listing(listingData);
-  await newListing.save();
-  res.redirect("/listings");
-});
+app.post(
+  "/listings",
+  validateListing,
+  wrapAsync(async (req, res, next) => {
+    let listingData = req.body.listing;
+    listingData.image = { url: listingData.image };
+    let newListing = new Listing(listingData);
+    await newListing.save();
+    res.redirect("/listings");
+  })
+);
 
 //--------- EDIT ROUTE -----------------------------------------
-app.get("/listings/:id/edit", async (req, res) => {
-  let { id } = req.params;
-  const listing = await Listing.findById(id);
-  res.render("listings/edit-listing.ejs", { listing });
-});
+app.get(
+  "/listings/:id/edit",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit-listing.ejs", { listing });
+  })
+);
 
 //--------- UPDATE ROUTE ---------------------------------------
-app.put("/listings/:id", async (req, res) => {
-  let { id } = req.params;
-  let updatedListing = { ...req.body.listing };
-  updatedListing.image = { url: req.body.listing.image };
-  await Listing.findByIdAndUpdate(id, updatedListing);
-  res.redirect(`/listings/${id}`);
-});
+app.put(
+  "/listings/:id",
+  validateListing,
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    let updatedListing = { ...req.body.listing };
+    updatedListing.image = { url: req.body.listing.image };
+    await Listing.findByIdAndUpdate(id, updatedListing);
+    res.redirect(`/listings/${id}`);
+  })
+);
 
 //----------- DELETE ROUTE ---------------------------------------
-app.delete("/listings/:id", async (req, res) => {
-  let { id } = req.params;
-  let deletedListing = await Listing.findByIdAndDelete(id);
-  console.log(deletedListing);
-  res.redirect("/listings");
-});
+app.delete(
+  "/listings/:id",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    res.redirect("/listings");
+  })
+);
 
 //----------- SHOW ROUTE ---------------------------------------
-app.get("/listings/:id", async (req, res) => {
-  let { id } = req.params;
-  const listing = await Listing.findById(id);
-  res.render("listings/show-listing.ejs", { listing });
-});
+app.get(
+  "/listings/:id",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show-listing.ejs", { listing });
+  })
+);
 
 // app.get("/testListing", async (req, res) => {
 //   let sampleListing = new Listing({
@@ -98,6 +133,15 @@ app.get("/listings/:id", async (req, res) => {
 //   console.log("sample was saved");
 //   res.send("successfule testing");
 // });
+
+app.all(/.*/, (req, res, next) => {
+  next(new ExpressError(404, "Page not found!!!"));
+});
+
+app.use((err, req, res, next) => {
+  let { statusCode = 500, message = "Something went wrong" } = err;
+  res.status(statusCode).render("Error.ejs", { message });
+});
 
 app.listen(PORT, () => {
   console.log(`app is listenning to ${PORT}`);
