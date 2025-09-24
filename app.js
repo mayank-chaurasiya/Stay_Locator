c5e45effd421345b9f0664e98e47ac2703461350
Mayank <mayankchaurasiya66@gmail.com>
2025-09-24 23:17:32 +0530

    updated how image upload works

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ async function main() {
 const store = MongoStore.create({
   mongoUrl: dbUrl,
   crypto: {
-    secret: "secrettt",
+    secret: process.env.SECRET,
   },
   touchAfter: 24 * 3600,
 });
@@ -56,7 +56,7 @@ store.on("error", () => {
 
 const sessionOptions = {
   store: store,
-  secret: "secrettt",
+  secret: process.env.SECRET,
   resave: false,
   saveUninitialized: true,
   cookie: {
